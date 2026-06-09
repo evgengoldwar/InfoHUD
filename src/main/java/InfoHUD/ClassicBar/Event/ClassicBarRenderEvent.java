6d3dc8fca5805fa99ecdefc6b4450c2ace7cf3ab
evgengoldwar <evgeniy10102003kuptsov@gmail.com>
2026-06-09 18:15:34 +0400

    Fix nei render bug

--- src/main/java/InfoHUD/ClassicBar/Event/ClassicBarRenderEvent.java
+++ src/main/java/InfoHUD/ClassicBar/Event/ClassicBarRenderEvent.java
@@ -83,6 +83,12 @@ public class ClassicBarRenderEvent {
         updateAllProgress();
         renderAllBars();
 
+        GL11.glColor4f(1F, 1F, 1F, 1F);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
         GL11.glPopMatrix();
     }
 
